515f1acb2256d6ae5cdd38fa1fcd19e0854ea55e
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-16 12:06:35 +0530

    modify users table to store firebase uid

--- app/models/user.py
+++ app/models/user.py
@@ -6,9 +6,10 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    firebase_uid= Column(
+        String(128), unique=True, index=True, nullable= False
+    )
     name = Column(String(100), nullable= False)
     email = Column(String(100), unique=True, index=True, nullable= False)
-    password_hash = Column(String(255))
     role = Column(Enum(UserRole), default=UserRole.NORMAL)
     is_verified = Column(Boolean, default=False)
-    refresh_token= Column(String(500), nullable= True)
