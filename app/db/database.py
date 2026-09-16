c3d0d2856b8a43e976aeedef5d0d10f77f7cb106
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-16 12:02:04 +0530

    migrate database to postgresql

--- app/db/database.py
+++ app/db/database.py
@@ -1,26 +1,17 @@
-from sqlalchemy import create_engine, event
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.core.config import settings
 
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True
+)
 
-engine=create_engine(settings.DATABASE_URL, echo= True, pool_pre_ping=True, connect_args={
-    "charset": "utf8mb4",
-    "use_unicode": True
-})
+SessionLocal = sessionmaker(
+    autoflush=False,
+    bind=engine
+)
 
-@event.listens_for(engine, "connect")
-def set_mysql_encoding(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET NAMES utf8mb4")
-    cursor.close()
-
-@event.listens_for(engine, "connect")
-def set_names(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET NAMES utf8mb4")
-    cursor.close()
-    
-SessionLocal= sessionmaker(autoflush=False, bind=engine)
-
-Base= declarative_base()
+Base = declarative_base()
