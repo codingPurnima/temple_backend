4b66cccf8d54abb7a8bce9a63a7fda2f2f973f61
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-17 10:30:18 +0530

    add verification field to user response schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,20 +1,12 @@
 from pydantic import BaseModel, EmailStr
 from app.core.config import settings
 
-class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
 class UserOut(BaseModel):
     id: int
     name: str
     email: str
     role: str
+    is_verified: bool
 
     class Config:
         from_attributes = True
