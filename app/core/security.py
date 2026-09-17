e1bcb6a85a30fabe48d55fdc51ed0f21fb9495cc
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-17 10:35:01 +0530

    remove legacy code

--- app/core/security.py
+++ app/core/security.py
@@ -1,51 +1,51 @@
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
-from app.core.config import settings
-from fastapi import HTTPException
+# from passlib.context import CryptContext
+# from datetime import datetime, timedelta
+# from jose import jwt, JWTError
+# from app.core.config import settings
+# from fastapi import HTTPException
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+# def hash_password(password: str):
+#     return pwd_context.hash(password)
 
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+# def verify_password(plain, hashed):
+#     return pwd_context.verify(plain, hashed)
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
+# def create_access_token(data: dict):
+#     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
+#     expire = datetime.utcnow() + timedelta(
+#         minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+#     )
 
-    to_encode.update({"exp": expire, "type": "access"})
+#     to_encode.update({"exp": expire, "type": "access"})
 
-    return jwt.encode(
-        to_encode,
-        settings.ACCESS_TOKEN_SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
+#     return jwt.encode(
+#         to_encode,
+#         settings.ACCESS_TOKEN_SECRET_KEY,
+#         algorithm=settings.ALGORITHM
+#     )
 
-def create_refresh_token(data: dict):
-    to_encode= data.copy()
-    expire= datetime.utcnow()+timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
-    to_encode.update({"exp": expire, "type": "refresh"})
+# def create_refresh_token(data: dict):
+#     to_encode= data.copy()
+#     expire= datetime.utcnow()+timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+#     to_encode.update({"exp": expire, "type": "refresh"})
 
-    return jwt.encode(
-        to_encode, 
-        settings.REFRESH_TOKEN_SECRET_KEY, 
-        algorithm=settings.ALGORITHM
-    )
+#     return jwt.encode(
+#         to_encode, 
+#         settings.REFRESH_TOKEN_SECRET_KEY, 
+#         algorithm=settings.ALGORITHM
+#     )
 
-def decode_refresh_token(token: str):
-    try:
-        payload= jwt.decode(token, settings.REFRESH_TOKEN_SECRET_KEY, algorithms=[settings.ALGORITHM])
+# def decode_refresh_token(token: str):
+#     try:
+#         payload= jwt.decode(token, settings.REFRESH_TOKEN_SECRET_KEY, algorithms=[settings.ALGORITHM])
 
-        if payload.get("type")!= "refresh":
-            raise HTTPException(status_code=401, detail="Invalid token type")
+#         if payload.get("type")!= "refresh":
+#             raise HTTPException(status_code=401, detail="Invalid token type")
         
-        return payload
+#         return payload
     
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid refresh token")
+#     except JWTError:
+#         raise HTTPException(status_code=401, detail="Invalid refresh token")
--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -1,17 +1,17 @@
-from uuid import uuid4
-from fastapi import UploadFile
-from app.firebase.firebase import bucket
+# from uuid import uuid4
+# from fastapi import UploadFile
+# from app.core.firebase import bucket
 
 
-def upload_file(file: UploadFile):
-    file_ext = file.filename.split(".")[-1]
-    unique_name = f"{uuid4()}.{file_ext}"
+# def upload_file(file: UploadFile):
+#     file_ext = file.filename.split(".")[-1]
+#     unique_name = f"{uuid4()}.{file_ext}"
 
-    blob = bucket.blob(unique_name)
+#     blob = bucket.blob(unique_name)
 
-    blob.upload_from_file(file.file, content_type=file.content_type)
+#     blob.upload_from_file(file.file, content_type=file.content_type)
 
-    # make public (for demo simplicity)
-    blob.make_public()
+#     # make public (for demo simplicity)
+#     blob.make_public()
 
-    return blob.public_url
+#     return blob.public_url
