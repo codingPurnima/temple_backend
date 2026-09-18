30ad86cfaea51fc02d788ae9a504e6eed6654082
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-18 21:55:33 +0530

    Create Super Admin in new database

--- app/scripts/create_super_admin.py
+++ app/scripts/create_super_admin.py
@@ -1,17 +1,43 @@
 from app.db.database import SessionLocal
 from app.models.user import User, UserRole
-from app.core.security import hash_password
+from firebase_admin import auth
+from app.core import firebase
 
+from app.core.config import settings
 db = SessionLocal()
 
-admin = User(
-    name="Jai Jai",
-    email="jaijai@girdharlal.com",
-    password_hash=hash_password("vrindavan"),
-    role=UserRole.SUPER_ADMIN
-)
+email= settings.SUPER_ADMIN_EMAIL
+password= settings.SUPER_ADMIN_PASSWORD
+name= settings.SUPER_ADMIN_NAME
 
-db.add(admin)
-db.commit()
+try:
+    firebase_user= auth.create_user(
+        email= email,
+        password= password,
+        display_name= name,
+        email_verified= True
+    )
 
-print("Super admin created")
+    print("Firebase user created.")
+    print("Firebase UID:", firebase_user.uid)
+
+    admin= User(
+        firebase_uid= firebase_user.uid,
+        name= name,
+        email= email,
+        role= UserRole.SUPER_ADMIN,
+        is_verified= True
+    )
+
+    db.add(admin)
+    db.commit()
+    db.refresh(admin)
+
+    print("Super admin created successfully")
+
+except Exception as e:
+    db.rollback()
+    print("Error:", e)
+
+finally:
+    db.close()
