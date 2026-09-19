f820ac9d41e573bdda94388d5c5bc5a37412595d
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-19 12:49:36 +0530

    fix render variables for deployment

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,6 +1,16 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("app/firebase/serviceAccountKey.json")
+if "FIREBASE_SERVICE_ACCOUNT_JSON" in os.environ:
+    firebase_credentials = json.loads(
+        os.environ["FIREBASE_SERVICE_ACCOUNT_JSON"]
+    )
+    cred = credentials.Certificate(firebase_credentials)
+else:
+    cred = credentials.Certificate(
+        "app/firebase/serviceAccountKey.json"
+    )
 
 firebase_admin.initialize_app(cred)
